17666052a140d7ec5a3b78e965d94f8041b941c5
Stefanos David Georgiou <77331068+Ogstef@users.noreply.github.com>
2021-11-01 15:44:14 +0200

    Add files via upload

--- Hey_there1.sql
+++ Hey_there1.sql
@@ -1,3 +1,6 @@
 select projno , projname , projmgrid, budget, startdate, expecteddurationweeks 
 from project
 where projname like '%urn%';
+
+select empid, firstname, lastname , (salary/12) as monthly_salary 
+from worker;
