43f70d29be6db19efc485fbaa51e08d8f91be3df
Stefanos David Georgiou <77331068+Ogstef@users.noreply.github.com>
2021-11-01 15:45:57 +0200

    Delete Hey_there1.sql

--- Hey_there1.sql
+++
@@ -1,6 +0,0 @@
-select projno , projname , projmgrid, budget, startdate, expecteddurationweeks 
-from project
-where projname like '%urn%';
-
-select empid, firstname, lastname , (salary/12) as monthly_salary 
-from worker;
