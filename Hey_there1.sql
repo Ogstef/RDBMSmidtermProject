371c033f14227869404b71dd398c6782462be3d1
Stefanos David Georgiou <77331068+Ogstef@users.noreply.github.com>
2021-11-04 23:25:15 +0200

    q4 added

--- Hey_there1.sql
+++ Hey_there1.sql
@@ -4,3 +4,11 @@ where projname like '%urn%';
 
 select empid, firstname, lastname , (salary/12) as monthly_salary 
 from worker;
+
+
+select firstname, lastname 
+from worker 
+join assign using (empid)
+join project using (projno)
+where projno = '1001'
+order by worker.lastname asc;
