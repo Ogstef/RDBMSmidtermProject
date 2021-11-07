ed6ba10500c7509d28a1f6b20211a86bd4ba60f3
Stefanos David Georgiou <77331068+Ogstef@users.noreply.github.com>
2021-11-07 13:39:14 +0200

    questions 2-10
    (no.8 needs some fixing, wont work and cant find solution..)

--- Hey_there1.sql
+++ Hey_there1.sql
@@ -1,12 +1,12 @@
-select projno , projname , projmgrid, budget, startdate, expecteddurationweeks 
+2. select projno , projname , projmgrid, budget, startdate, expecteddurationweeks 
 from project
 where projname like '%urn%';
 
-select empid, firstname, lastname , (salary/12) as monthly_salary 
+3. select empid, firstname, lastname , (salary/12) as monthly_salary 
 from worker;
 
 
-select firstname, lastname 
+4. select firstname, lastname 
 from worker 
 join assign using (empid)
 join project using (projno)
@@ -15,6 +15,32 @@ order by worker.lastname asc;
 
 
 
-select firstname, lastname, salary, departmentid 
+5. select firstname, lastname, salary, departmentid 
 from worker 
 where departmentid = '2' and salary = (select min(salary) from worker);
+
+
+6. select * 
+from project 
+where budget = (select max(budget) from project );
+
+7. select firstname, lastname , departmentid
+from worker
+join assign on ( worker.empid = assign.empid )
+where projno ='1019';
+
+8. select firstname, lastname , rating 
+from worker
+join project on ( worker.empid = project.projmgrid )
+join assign on ( project.projno = project.projno )
+join worker on ( assign.empid = worker.empid)
+where firstname != 'Micheal' and lastname != 'Burns';
+
+9. create view "myNewView" as 
+select projno, projname, firstname, lastname, empid
+from worker, project;
+
+10.
+select projno, projname 
+from "myNewView"
+where empid = '101';
