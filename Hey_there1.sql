3ed3888d9127a08c59352480e9f9e09d03f4ba6a
Stefanos David Georgiou <77331068+Ogstef@users.noreply.github.com>
2021-11-07 15:21:33 +0200

    Update Hey_there1.sql

--- Hey_there1.sql
+++ Hey_there1.sql
@@ -1,62 +1,62 @@
-2. select projno , projname , projmgrid, budget, startdate, expecteddurationweeks 
-from project
-where projname like '%urn%';
+2. SELECT projno , projname , projmgrid, budget, startdate, expecteddurationweeks 
+FROM project
+WHERE projname LIKE '%urn%';
 
-3. select empid, firstname, lastname , (salary/12) as monthly_salary 
-from worker;
+3. SELECT empid, firstname, lastname , (salary/12) AS monthly_salary 
+FROM worker;
 
 
-4. select firstname, lastname 
-from worker 
-join assign using (empid)
-join project using (projno)
-where projno = '1001'
-order by worker.lastname asc;
+4. SELECT firstname, lastname 
+FROM worker 
+JOIN assign using (empid)
+JOIN project using (projno)
+WHERE projno = '1001'
+ORDER BY worker.lastname ASC;
 
 
 
 5.SELECT firstname, lastname, salary, departmentid 
 FROM worker 
-WHERE departmentid = 2 AND salary = (SELECT MIN(salary) FROM worker where departmentid = 2);
+WHERE departmentid = 2 AND salary = (SELECT MIN(salary) FROM worker WHERE departmentid = 2);
 
 
-6. select * 
-from project 
-where budget = (select max(budget) from project );
+6. SELECT * 
+FROM project 
+WHERE budget = (SELECT max(budget) FROM project );
 
-7. select firstname, lastname , departmentid
-from worker
-join assign on ( worker.empid = assign.empid )
-where projno ='1019';
+7. SELECT firstname, lastname , departmentid
+FROM worker
+JOIN assign ON ( worker.empid = assign.empid )
+WHERE projno ='1019';
 
-8. select firstname, lastname , rating 
-from worker
-join project on ( worker.empid = project.projmgrid )
-join assign on ( project.projno = project.projno )
-join worker on ( assign.empid = worker.empid)
-where firstname != 'Micheal' and lastname != 'Burns';
+8. SELECT firstname, lastname , rating 
+FROM worker
+JOIN project ON ( worker.empid = project.projmgrid )
+JOIN assign ON ( project.projno = project.projno )
+join worker ON ( assign.empid = worker.empid)
+WHERE firstname != 'Micheal' AND lastname != 'Burns';
 
-9. create view "myNewView" as 
-select projno, projname, firstname, lastname, empid
-from worker, project;
+9. CREATE view "myNewView" AS 
+SELECT projno, projname, firstname, lastname, empid
+FROM worker, project;
 
 10.
-select projno, projname 
-from "myNewView"
-where empid = '101';
+SELECT projno, projname 
+FROM "myNewView"
+WHERE empid = '101';
 
 11. 
-insert into worker(empid, lastname, firstname, departmentid, birthdate, hiredate, salary ) values ( 201841 , 'Georgiou', 'Stefanos', 2 , '13-APR-1999', '15-APR-2021', 75000);
-insert into assign ( projno , empid, hoursassigned, rating ) values ( '1001' , '201841', '15', '3');
-insert into assign ( projno , empid, hoursassigned, rating ) values ( '1030' , '201841', '15', '3');
-insert into worker(empid, lastname, firstname, departmentid, birthdate, hiredate, salary ) values ( 201869 , 'Macris', 'Constantine', 2 , '05-JUN-2000', '27-MAR-2021', 100000);
-insert into assign ( projno , empid, hoursassigned, rating ) values ( '1001' , '201869', '20', '5');
-insert into assign ( projno , empid, hoursassigned, rating ) values ( '1025' , '201869', '15', '3');
-select * from "myNewView";
+INSERT INTO worker(empid, lastname, firstname, departmentid, birthdate, hiredate, salary ) VALUES ( 201841 , 'Georgiou', 'Stefanos', 2 , '13-APR-1999', '15-APR-2021', 75000);
+INSERT INTO assign ( projno , empid, hoursassigned, rating ) VALUES ( '1001' , '201841', '15', '3');
+INSERT INTO assign ( projno , empid, hoursassigned, rating ) VALUES ( '1030' , '201841', '15', '3');
+INSERT INTO worker(empid, lastname, firstname, departmentid, birthdate, hiredate, salary ) VALUES ( 201869 , 'Macris', 'Constantine', 2 , '05-JUN-2000', '27-MAR-2021', 100000);
+INSERT INTO assign ( projno , empid, hoursassigned, rating ) VALUES ( '1001' , '201869', '20', '5');
+INSERT INTO assign ( projno , empid, hoursassigned, rating ) VALUES ( '1025' , '201869', '15', '3');
+SELECT * FROM "myNewView";
 
 12. 
-update assign 
-set hoursassigned =10 
-where assign.empid = '110' and assign.projno = '1019';
+UPDATE assign 
+SET hoursassigned =10 
+WHERE assign.empid = '110' AND assign.projno = '1019';
 
 
